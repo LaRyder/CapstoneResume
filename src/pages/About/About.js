e912ff94acf5b54cc01da571fc789ef693d72b56
Lauren Ryder <lauren.ryder@microsoft.com>
2021-12-18 15:02:23 -0500

    Adding more images

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,13 +1,30 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import ReactDom from "react-dom";
 import "./About.css";
+import Coverflow from "react-coverflow"
+import pic1 from "."
 
-function About() {
-    return (
-     <div className="about">
-        <h1><strong>All about me</strong></h1>
-        </div>
-    );
-} 
- 
-export default About;
+var fn = function() {
+
+}
+
+ReactDOM.render(
+    <Coverflow>
+    width={960}
+    height={480}
+    displayQuantityOfSide={2}
+    navigation={false}
+    enableHeading={false}
+
+    <div>
+        onClick={() => fn()}
+        onKeyDown={() => fn()}
+        role="meneitem"
+        tabIndex="0"
+
+    </div>
+
+    </Coverflow>,
+
+    document.querySelector(".content")
+);
