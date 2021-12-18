567eefe04e6f2c69dfedfef59d4a2c5b9992df8e
Lauren Ryder <lauren.ryder@microsoft.com>
2021-12-18 18:13:12 -0500

    adding carousel to about me page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,26 +2,40 @@ import React from "react";
 import "./About.css"
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
+import pic1 from "../../Images/13141.jpeg"
+import pic2 from "../../Images/36150.jpg"
+import pic3 from "../../Images/67433.jpg"
+import pic4 from "../../Images/87125.jpg"
+import pic5 from "../../Images/20201123.jpg"
+import pic6 from "../../Images/20201224.jpg"
+import pic7 from "../../Images/20211009.jpg"
+import pic8 from "../../Images/FB_IMG_1.jpg"
+import pic9 from "../../Images/Screenshot_2.jpg"
+import pic10 from "../../Images/Snapchat-9.jpg"
+import pic11 from "../../Images/Snapchat-13.jpg"
+import pic12 from "../../Images/Snapchat-16.jpg"
+import pic13 from "../../Images/Snapchat-17.jpg"
+import pic14 from "../../Images/Snapchat-20.jpg"
 
 function About() {
     return (
         <div className="carousel">
             <AliceCarousel autoPlay autoPlayInterval="3000">
-                <img src="src/Images/13141.jpeg" alt="My family"/>
-                <img src="src/Images/36150.jpg" alt="My family"/>
-                <img src="src/Images/67433.jpg" alt="My family"/>
-                <img src="src/Images/20201123.jpg" alt="My family"/>
-                <img src="src/Images/20201224.jpg" alt="My family"/>
-                <img src="src/Images/20211009.jpg" alt="My family"/>
-                <img src="src/Images/FB_IMG_1.jpg" alt="My family"/>
-                <img src="src/Images/Screenshot_2.jpg" alt="My family"/>
-                <img src="src/Images/Snapchat-9.jpg" alt="My family"/>
-                <img src="src/Images/Snapchat-13.jpg" alt="My family"/>
-                <img src="src/Images/Snapchat-16.jpg" alt="My family"/>
-                <img src="src/Images/87125.jpg" alt="My family"/>
-                <img src="src/Images/Snapchat-17.jpg" alt="My family"/>
-                <img src="src/Images/Snapchat-20.jpg" alt="My family"/> 
-            </AliceCarousel>
+                <img src={pic1} alt="My family" class="center-block"/>
+                <img src={pic2} alt="My family"/>
+                <img src={pic3} alt="My family"/>
+                <img src={pic4} alt="My family"/>
+                <img src={pic5} alt="My family"/>
+                <img src={pic6} alt="My family"/>
+                <img src={pic7} alt="My family"/>
+                <img src={pic8} alt="My family"/>
+                <img src={pic9} alt="My family"/>
+                <img src={pic10} alt="My family"/>
+                <img src={pic11} alt="My family"/>
+                <img src={pic12} alt="My family"/>
+                <img src={pic13} alt="My family"/>
+                <img src={pic14} alt="My family"/>
+              </AliceCarousel>
         </div>
     );
 }
