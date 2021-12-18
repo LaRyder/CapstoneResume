e3cdae4ed0c23228c7fa89a6030047ee3deaea83
Lauren Ryder <lauren.ryder@microsoft.com>
2021-12-18 16:49:49 -0500

    Adding more images

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,11 +7,23 @@ function About() {
     return (
         <div className="carousel">
             <AliceCarousel autoPlay autoPlayInterval="3000">
-                <img src="src/Images/20201123_165438.jpg" alt="My family"/>
-                
+                <img src="src/Images/13141.jpeg" alt="My family"/>
+                <img src="src/Images/36150.jpg" alt="My family"/>
+                <img src="src/Images/67433.jpg" alt="My family"/>
+                <img src="src/Images/20201123.jpg" alt="My family"/>
+                <img src="src/Images/20201224.jpg" alt="My family"/>
+                <img src="src/Images/20211009.jpg" alt="My family"/>
+                <img src="src/Images/FB_IMG_1.jpg" alt="My family"/>
+                <img src="src/Images/Screenshot_2.jpg" alt="My family"/>
+                <img src="src/Images/Snapchat-9.jpg" alt="My family"/>
+                <img src="src/Images/Snapchat-13.jpg" alt="My family"/>
+                <img src="src/Images/Snapchat-16.jpg" alt="My family"/>
+                <img src="src/Images/87125.jpg" alt="My family"/>
+                <img src="src/Images/Snapchat-17.jpg" alt="My family"/>
+                <img src="src/Images/Snapchat-20.jpg" alt="My family"/> 
             </AliceCarousel>
         </div>
-    )
+    );
 }
 
 export default About;
