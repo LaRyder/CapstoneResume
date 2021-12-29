5a2e889faca567ab84bf38cd2c6775eb6ffbabe0
Lauren Ryder <lauren.ryder@microsoft.com>
2021-12-29 13:42:52 -0500

    adding favorite quotes

--- src/pages/Resume/Header.js
+++ src/pages/Resume/Header.js
@@ -5,6 +5,7 @@ export default class Header extends React.Component {
         return (
         <div class="top">
             <h1 className='header'>LAUREN RYDER RESUME</h1>
+            <h2 className='quote'>“Choose a job you love, and you will never have to work a day in your life. ” — Confucius</h2>
             <p>Deployment Technician / Leap Apprentice</p>
             <p>Over 6 years of experience as a solution driven information technology professional with proven education and experience while delivering projects and throughput well within targeted SLAs.</p>
         </div>
