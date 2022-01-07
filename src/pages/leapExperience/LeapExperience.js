7b86420a0de0653ed8e24d23a6b70d67245a3754
Lauren Ryder <lauren.ryder@microsoft.com>
2022-01-07 10:57:42 -0500

    adding live links

--- src/pages/leapExperience/LeapExperience.js
+++ src/pages/leapExperience/LeapExperience.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./LeapExperience.css";
 import {FaGithub} from 'react-icons/fa';
 import LeapBackground from './LeapBackground'
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 export default class LeapExperience extends React.Component {
     render () {
@@ -20,30 +22,20 @@ export default class LeapExperience extends React.Component {
                     <p>I learned how to create fun games, such as tic-tac-toe and hangman, create and consume APIs, create SQL applications, as well as build a quilt using C#, </p>
                     <p>all of which can be found in my Githud <a href="https://github.com/LaRyder"> <FaGithub></FaGithub ></a>. </p>
                     <p>The program is a fast-paced, challenging, and very well planned curriculum that has provided and solid foundation to start my dream career. </p>
-
-                    <ul className="links">
-                        {/* Links pulled from Leap GitHub */}
-                        <li><a href="https://www.linkedin.com/learning/git-essential-training-the-basics?u=3322">Git Essential Training: The Basics</a></li>
-                        <li><a href="https://www.linkedin.com/learning/html-essential-training-4?u=3322">HTML Essential Training</a></li>
-                        <li><a href="https://www.linkedin.com/learning/git-branches-merges-and-remotes?u=3322">Git: Branches, Merges, and Remotes</a></li>
-                        <li><a href="https://www.linkedin.com/learning/introduction-to-css?u=3322">Introduction to CSS</a></li>
-                        <li><a href="https://www.linkedin.com/learning/overcoming-imposter-syndrome?u=3322">Overcoming Imposter Syndrome</a></li>
-                        <li><a href="https://www.linkedin.com/learning/c-sharp-essential-training-1-syntax-and-object-oriented-programming?u=3322">C# Essential Training: 1 Syntax and Object Oriented Programming</a></li>
-                        <li><a href="https://www.linkedin.com/learning/c-sharp-essential-training-2-flow-control-arrays-and-exception-handling?u=3322">C# Essential Training: 2 Flow Control, Arrays, and Exception Handling</a></li>
-                        <li><a href="https://www.linkedin.com/learning/programming-foundations-object-oriented-design-3?u=3322">C# Test Driven Development (2016)</a></li>
-                        <li><a href="https://www.linkedin.com/learning/c-sharp-test-driven-development-2016?u=3322">C# Test Driven Development (2016)</a></li>
-                        <li><a href="https://www.linkedin.com/learning/learning-c-sharp-algorithms?u=3322">Learning C# Algorithms</a></li>
-                        <li><a href="https://www.linkedin.com/learning/learning-sql-programming-8382385?u=3322">Learning SQL Programming</a></li>
-                        <li><a href="https://www.linkedin.com/learning/sql-code-challenges?u=3322">SQL Code Challenges</a></li>
-                        <li><a href="https://www.linkedin.com/learning/c-sharp-advanced-practices?u=3322">C#: Advanced Practices</a></li>
-                        <li><a href="https://www.linkedin.com/learning/building-web-apis-with-asp-dot-net-core-3?u=3322">Building Web APIs with ASP.NET Core</a></li>
-                        <li><a href="https://www.linkedin.com/learning/learning-the-javascript-language-2?u=3322">Learning the JavaScript Language</a></li>
-                        <li><a href="https://www.linkedin.com/learning/react-js-essential-training?u=3322">React.js Essential Training</a></li>
-                        <li><a href="https://www.linkedin.com/learning/javascript-essential-training?u=3322">JavaScript Essential Training</a></li>
-                        <li><a href="https://www.linkedin.com/learning/react-building-styles-with-css-modules-9222678?u=3322">React: Building Styles with CSS Modules</a></li>
-                        <li><a href="https://www.linkedin.com/learning/learning-the-javascript-language-2?u=3322">Learning the JavaScript Language</a></li>
-                        <li><a href="https://www.linkedin.com/learning/react-working-with-apis?u=3322">React: Working with APIs</a></li>
-                    </ul>
+                    
+                    <Popup trigger={<button className="tTrainings">Technical Trainings</button>}>
+                        <div class="text"><a href="https://www.linkedin.com/learning/introduction-to-css?u=3322">~ Introduction to CSS</a></div>
+                        <div class="text"><a href="https://www.linkedin.com/learning/html-essential-training-4?u=3322">~ HTML Essential Training</a></div>
+                        <div class="text"><a href="https://www.linkedin.com/learning/learning-c-sharp-algorithms?u=3322">~ Learning C# Algorithms</a></div>
+                        <div class="text"><a href="https://www.linkedin.com/learning/learning-sql-programming-8382385?u=3322">~ Learning SQL Programming</a></div>
+                        <div class="text"><a href="https://www.linkedin.com/learning/building-web-apis-with-asp-dot-net-core-3?u=3322">~ Building Web APIs with ASP.NET Core</a></div>
+                        <div class="text"><a href="https://www.linkedin.com/learning/learning-the-javascript-language-2?u=3322">~ Learning the JavaScript Language</a></div>
+                        <div class="text"><a href="https://www.linkedin.com/learning/react-building-styles-with-css-modules-9222678?u=3322">~ React: Building Styles with CSS Modules</a></div>
+                    </Popup>
+                    <Popup trigger={<button className="nTrainings">Non-Technical Trainings</button>}>
+                        <div class="text"><a href="https://www.linkedin.com/learning/overcoming-imposter-syndrome?u=3322">~ Overcoming Imposter Syndrome</a></div>
+                        <div class="text"><a href="https://www.linkedin.com/learning/c-sharp-essential-training-1-syntax-and-object-oriented-programming?u=3322">~ C# Essential Training: 1 Syntax and Object Oriented Programming</a></div>
+                    </Popup>
                 </div>
             </div>
         );
